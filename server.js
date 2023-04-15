0d43aa5ed900220e2264ab21ccc6e2e661158dec
ripp4rd0c <hououinkyoma13@protonmail.com>
2023-04-15 16:55:29 +0200

    basic init & router

---
+++ server.js
@@ -0,0 +1,41 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    const route = req.url;
+    // /api/books/:id   [GET PUT DELETE]
+    // /api/books/      [GET POST]
+
+    const pathToBook = /\/api\/books\/([0-9a-z\-]+)/;
+    const validRootPath = /\/api\/books\/?/;
+    let m, id;
+
+    if((m = route.match(pathToBook)) && req.method === 'GET'){
+        id = m[1]
+        getBookById(id, req, res);
+    }
+    else if ((m = route.match(pathToBook)) && req.method === 'PUT'){
+
+    }
+    else if ((m = route.match(pathToBook)) && req.method === 'DELETE'){
+
+    }
+    else if(route.match(validRootPath) && req.method === 'GET'){
+
+    }
+    else if (route.match(validRootPath) && req.method === 'POST') {
+
+    }
+
+
+    else{
+        res.writeHead(404, {
+            'content-type': 'application/javascript'
+        });
+        res.end(JSON.stringify({ message: "ROUTE NOT FOUND" }))
+    }
+});
+
+
+const PORT = process.env.PORT || 5000
+server.listen(PORT)
+
